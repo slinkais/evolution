46814a03e2398bd56327966d39352f51697f7912
Dzeralds Snebahs <k.dzeralds.snebahs@accenture.com>
2017-04-19 16:20:17 +0300

    Initially com

---
+++ sketch.js
@@ -0,0 +1,119 @@
+var vehicles = [];
+var food = [];
+var poison = [];
+
+var foodMax = 100;
+var poisonMax = 50;
+var vehicleMax = 1;
+
+var debug;
+
+function setup() {
+    createCanvas(innerWidth, innerHeight - 20);
+    foodMax = innerWidth * innerHeight * 0.0005;
+    poisonMax = innerWidth * innerHeight * 0.0002;
+    vehicleMax = innerWidth * innerHeight * 0.00002;
+    for (var i = 0; i < vehicleMax; i++) {
+        var x = random(width);
+        var y = random(height);
+        vehicles[i] = new Vehicle(x, y);
+    }
+
+    for (var i = 0; i < foodMax; i++) {
+        var x = random(width);
+        var y = random(height);
+        food.push(createVector(x, y));
+    }
+
+    for (var i = 0; i < poisonMax; i++) {
+        var x = random(width);
+        var y = random(height);
+        poison.push(createVector(x, y));
+    }
+
+    debug = createCheckbox('Debug Drawings');
+
+
+
+
+}
+
+function windowResized() {
+    resizeCanvas(innerWidth, innerHeight - 20);
+    foodMax = innerWidth * innerHeight * 0.0005;
+    poisonMax = innerWidth * innerHeight * 0.0002;
+    vehicleMax = innerWidth * innerHeight * 0.00002;
+
+    food = [];
+    poison = [];
+    for (var i = 0; i < foodMax; i++) {
+        var x = random(width);
+        var y = random(height);
+        food.push(createVector(x, y));
+    }
+
+    for (var i = 0; i < poisonMax; i++) {
+        var x = random(width);
+        var y = random(height);
+        poison.push(createVector(x, y));
+    }
+}
+
+function mouseDragged() {
+    vehicles.push(new Vehicle(mouseX, mouseY));
+}
+
+function draw() {
+    background(51);
+
+    if (food.length < foodMax && vehicles.length < vehicleMax) {
+        var x = random(width);
+        var y = random(height);
+        food.push(createVector(x, y));
+    }
+
+    if (poison.length < poisonMax) {
+        var x = random(width);
+        var y = random(height);
+        poison.push(createVector(x, y));
+    }
+
+    if (vehicles.length < 1) {
+        var x = random(width);
+        var y = random(height);
+        vehicles.push(new Vehicle(x, y));
+    }
+
+
+    for (var i = 0; i < food.length; i++) {
+        fill(0, 255, 0);
+        noStroke();
+        ellipse(food[i].x, food[i].y, 4, 4);
+    }
+
+    for (var i = 0; i < poison.length; i++) {
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(poison[i].x, poison[i].y, 4, 4);
+    }
+
+    for (var i = vehicles.length - 1; i >= 0; i--) {
+        vehicles[i].boundaries();
+        vehicles[i].behaviors(food, poison);
+        vehicles[i].update();
+        vehicles[i].display();
+
+        var newVehicle = vehicles[i].clone();
+        if (newVehicle != null) {
+            vehicles.push(newVehicle);
+        }
+
+        if (vehicles[i].dead()) {
+            var x = vehicles[i].position.x;
+            var y = vehicles[i].position.y;
+            food.push(createVector(x, y));
+            vehicles.splice(i, 1);
+        }
+
+    }
+}
